14c5e148055afed9e6197eb4f9cb3b8ef1243fe9
VanPhuc <dovanphuc24022007@gmail.com>
2026-04-25 01:24:11 +0700

    khôi phục core data sau merge

---
+++ src/main/java/com/auction/system/model/item/Item.java
@@ -0,0 +1,113 @@
+package com.auction.system.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Item {
+    private int id;
+    private String name, description;
+    private double startPrice, currentPrice;
+    private AuctionStatus status;
+    private String sellerId;
+    private String highestBidderId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private final List<Bid> bidHistory = new ArrayList<>();
+
+    public Item() {}
+
+    public Item(int id, String name, String description, double startPrice, double currentPrice, AuctionStatus status)  {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startPrice = startPrice;
+        this.currentPrice = currentPrice > 0 ? currentPrice : startPrice;
+        this.status = status != null ? status : AuctionStatus.OPEN;
+    }
+
+    public AuctionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AuctionStatus status) {
+        this.status = status;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public double getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(double startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getHighestBidderId() {
+        return highestBidderId;
+    }
+
+    public void setHighestBidderId(String highestBidderId) {
+        this.highestBidderId = highestBidderId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<Bid> getBidHistory() {
+        return Collections.unmodifiableList(bidHistory);
+    }
+
+    public void addBid(Bid bid) {
+        bidHistory.add(bid);
+    }
+}
