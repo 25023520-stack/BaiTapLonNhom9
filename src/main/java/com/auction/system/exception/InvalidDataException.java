5d5dace884e16f8b133b328b8c615d2f241ad6a1
VanPhuc <dovanphuc24022007@gmail.com>
2026-04-29 10:31:53 +0700

    Revert "Update auction system files"
    This reverts commit e4595ffce875cb0b480d07c7dbe10206f75d405f.

--- src/main/java/com/auction/system/exception/InvalidDataException.java
+++ src/main/java/com/auction/system/exception/InvalidDataException.java
@@ -1,5 +1,7 @@
 package com.auction.system.exception;
 
+//dữ liệu đầu vào sai
+
 public class InvalidDataException extends Exception { 
     public InvalidDataException(String message) {
         super(message);
