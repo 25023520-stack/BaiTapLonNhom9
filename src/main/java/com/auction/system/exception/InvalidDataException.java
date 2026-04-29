e4595ffce875cb0b480d07c7dbe10206f75d405f
VanPhuc <dovanphuc24022007@gmail.com>
2026-04-29 10:26:18 +0700

    Update auction system files

--- src/main/java/com/auction/system/exception/InvalidDataException.java
+++ src/main/java/com/auction/system/exception/InvalidDataException.java
@@ -1,7 +1,5 @@
 package com.auction.system.exception;
 
-//dữ liệu đầu vào sai
-
 public class InvalidDataException extends Exception { 
     public InvalidDataException(String message) {
         super(message);
